25496cd439e995f37919de1831bdd25c9eea58ff
NathanBitoun <nathanbitoun@gmail.com>
2025-01-20 12:13:25 +0100

    Bottes fin tp1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("lié avec succés !");
+const button1 = document.getElementById('btn1');
+console.log(button1);
+const button2 = document.getElementById('btn2');
+const button3 = document.getElementById('btn3');
+
+
+
+button1.addEventListener('click', () => { alert('Bottes de moto ajouté au panier') });
+button2.addEventListener('click', () => { alert('Bottes qui appartenait à ma grand mère ajouté au panier') });
+button3.addEventListener('click', () => { alert('Bottes bon marché ajouté au panier') });
